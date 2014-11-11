052927dba6e3626696b4afbed8ff2e5babc88290
AME <ame@anomes.com>
2014-11-11 23:56:10 +0100

    new : 4 & 5

---
+++ Program_4.ino
@@ -0,0 +1,14 @@
+void program4AtStep(int step)//4)un mode mort : clignotage de tout le costume en rouge (ou couleur originale)
+{
+  if  (step%5 > 1)
+  {
+    setColorForAllPixels( CRGB::Red);// ou setColorForAllPixels(COSTUME_COLOR);
+  }
+  else
+  {
+    setColorForAllPixels( CRGB::Black);
+  }
+  currentProgramStep ++;
+}
+
+
---
+++ Program_5.ino
@@ -0,0 +1,17 @@
+void program5AtStep(int step)//4) si on arrive à l'utiliser en simple impulsion pour les dégats reçu.
+{
+  if  (step< 500)//durée de l'impulsion 
+  {
+    setColorForAllPixels( CRGB::Red);// ou setColorForAllPixels(COSTUME_COLOR);
+  }
+  else
+  {
+    setColorForAllPixels( CRGB::Black);
+  }
+  currentProgramStep ++;
+}
+
+
+
+
+
