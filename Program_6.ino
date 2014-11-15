e1946f11e8431c15b58f31b59b762923f2b290db
AME <ame@anomes.com>
2014-11-15 12:35:12 +0100

    clean comments in prog6

--- Program_6.ino
+++ Program_6.ino
@@ -1,9 +1,9 @@
 void program6AtStep(int step)//Mode Boss full on + flash rouge + arlequin  
 {
-  int strobeSpeed = 10000; 
-  if  (step % strobeSpeed  > 0 && step % strobeSpeed  < 2000)
+  int strobeSpeed = 15000; // durée totale du programme 
+  if  (step % strobeSpeed  > 0 && step % strobeSpeed  < 2000) // 2000 = durée alernance rouge/noire
   {
-    int deathSpeed = 200;
+    int deathSpeed = 200;// vitesse de rouge/noire
     if  (step % deathSpeed  > 0 && step % deathSpeed  < deathSpeed/2)
     {
       setColorForAllPixels( CRGB::Red);// ou setColorForAllPixels(COSTUME_COLOR);
@@ -14,15 +14,14 @@ void program6AtStep(int step)//Mode Boss full on + flash rouge + arlequin
     }
   }
 
-  else if  (step % strobeSpeed  >= strobeSpeed/2 && step % strobeSpeed  < 7000)
+  else if  (step % strobeSpeed  >= 5000 && step % strobeSpeed  < 7000)//5000 temps ou commence l'arlequin | 7000 temps ou finit l'arlequin 
   {
-    if (currentProgramStep % 255 < 2 )
+    if (currentProgramStep % 255 < 2 )//255 vitesse de l'arlequin
     {
       setRandomColorForPatch();
       setRandomColorForRibs();
     }
   }
-
   else 
   {
     setColorForAllPixels(COSTUME_COLOR);
