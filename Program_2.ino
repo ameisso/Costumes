4e1149e25cec51680c7a296156eb34f0336e93b9
AME <ame@anomes.com>
2015-01-16 22:20:13 +0100

    New : 10/60 boutton rouge

--- Program_2.ino
+++ Program_2.ino
@@ -2,7 +2,7 @@ void program2AtStep(int step)//PHARE : plus lent qu'à lyon
 {
 
   Serial.println(step);
-  if  (breatheVariable < 20 || breatheVariable>200)
+  if  (breatheVariable < 20 || breatheVariable>150)
   {
     isBreatheAscending = !isBreatheAscending; 
   }
@@ -15,20 +15,20 @@ void program2AtStep(int step)//PHARE : plus lent qu'à lyon
   }
   else
   {
-    breatheVariable -= 5;
+    breatheVariable -= 10;
     FastLED.setBrightness( breatheVariable );
     setColorForAllPixels(COSTUME_COLOR);
   }
   delay (100);//vitesse du breathe
 
-  //debut flash
+ /* //debut flash
   int randomVar = random(40);//fréquence du flash
   if ( randomVar == 0)
   {
     setColorForAllPixels(COSTUME_COLOR,255);
     delay (30);
   }
-  //fin flash
+  //fin flash*/
 }
 
 //rouge
